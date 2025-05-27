84a1f64d6a4f452d07a78ef69c90aa7517061c96
Rishabh jain <91287627+Rishabh690@users.noreply.github.com>
2025-05-27 19:21:47 -0400

    Time: 0 ms (100.00%) | Memory: 12.5 MB (12.66%) - LeetSync

--- 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.py
+++ 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.py
@@ -1,12 +1,18 @@
 class Solution(object):
     def differenceOfSums(self, n, m):
-        num1=0
-        num2=0
-        for i in range(1,n+1):
-            if i % m == 0:
-                num2 += i
-            else:
-                num1 += i 
-        return num1-num2
+        # NEW APPROACH
+        total_sum = n*(n+1) // 2
+        k= n//m
+        divisible_sum = k * m *(k+1) // 2
+        return (total_sum - divisible_sum) - divisible_sum
+        # OLD APPROACH
+        # num1=0
+        # num2=0
+        # for i in range(1,n+1):
+        #     if i % m == 0:
+        #         num2 += i
+        #     else:
+        #         num1 += i 
+        # return num1-num2
 
         

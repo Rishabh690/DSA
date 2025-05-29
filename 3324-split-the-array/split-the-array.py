00fbf208596fe6bcc0d69bdd63be35a61726b0b1
Rishabh jain <91287627+Rishabh690@users.noreply.github.com>
2025-05-29 16:10:35 -0400

    Time: 0 ms (100.00%) | Memory: 12.5 MB (5.36%) - LeetSync

--- 3324-split-the-array/split-the-array.py
+++ 3324-split-the-array/split-the-array.py
@@ -1,13 +1,6 @@
 class Solution(object):
     def isPossibleToSplit(self, nums):
-        n=len(nums)
-        nums1=[]
-        nums2= []
-        if n == 0:
-            return False 
-        if n%2 != 0:
-            return False
-        first_element = nums[0]
+       
         freq = Counter(nums)
         for count in freq.values():
             if count > 2:

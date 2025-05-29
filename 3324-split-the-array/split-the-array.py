646e3e9c5f1bc177daaba34d11ae80e142c7e590
Rishabh jain <91287627+Rishabh690@users.noreply.github.com>
2025-05-29 16:12:22 -0400

    Time: 0 ms (100.00%) | Memory: 12.4 MB (50.00%) - LeetSync

--- 3324-split-the-array/split-the-array.py
+++ 3324-split-the-array/split-the-array.py
@@ -2,7 +2,6 @@ class Solution(object):
     def isPossibleToSplit(self, nums):
        
         freq = Counter(nums)
-        print(freq)
         for count in freq.values():
             if count > 2:
                 return False

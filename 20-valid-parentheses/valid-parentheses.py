6dd1847eba76f0a65cc2b0df292e1790333ac578
Rishabh jain <91287627+Rishabh690@users.noreply.github.com>
2025-06-03 20:11:48 -0400

    Time: 2 ms (61.22%) | Memory: 12.5 MB (56.80%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,14 +1,16 @@
 class Solution(object):
     def isValid(self, s):
-        result = []
-        closeToOpen = {")":"(","]":"[","}":"{"}
+        result=[]
+        poojan = {")":"(","]":"[","}":"{"}
 
-        for i in s :
-            if i in closeToOpen: 
-                if result and result[-1] ==closeToOpen[i]:
+        for i in s:
+            if i in poojan:
+                if result and result[-1] == poojan[i]:
                     result.pop()
                 else:
                     return False
             else:
                 result.append(i)
         return True if not result else False
+        
+        

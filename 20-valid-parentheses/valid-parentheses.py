30f7d42d3d0a99f3e5851f03f684c81e4b5d281c
Rishabh jain <91287627+Rishabh690@users.noreply.github.com>
2025-06-03 20:12:48 -0400

    Time: 4 ms (27.89%) | Memory: 12.5 MB (56.80%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,16 +1,31 @@
 class Solution(object):
     def isValid(self, s):
-        result=[]
-        poojan = {")":"(","]":"[","}":"{"}
-
+        l = []
         for i in s:
-            if i in poojan:
-                if result and result[-1] == poojan[i]:
-                    result.pop()
-                else:
-                    return False
+            if i == '(' or i == '{' or i == '[':
+                l.append(i)
+            elif i == ')' and l and l[-1] == '(':
+                l.pop()
+            elif i == '}' and l and l[-1] == '{':
+                l.pop()
+            elif i == ']' and l and l[-1] == '[':
+                l.pop()
             else:
-                result.append(i)
-        return True if not result else False
+                return False
+        return not l
+            
+        
+        # result=[]
+        # poojan = {")":"(","]":"[","}":"{"}
+
+        # for i in s:
+        #     if i in poojan:
+        #         if result and result[-1] == poojan[i]:
+        #             result.pop()
+        #         else:
+        #             return False
+        #     else:
+        #         result.append(i)
+        # return True if not result else False
         
         

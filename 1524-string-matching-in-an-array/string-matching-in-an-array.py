16680390977092ee457689dee7232ed69e369e26
Rishabh jain <91287627+Rishabh690@users.noreply.github.com>
2025-06-04 17:20:03 -0400

    Time: 18 ms (5.15%) | Memory: 12.5 MB (10.50%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.py
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.py
@@ -3,10 +3,11 @@ class Solution(object):
         result=[]
         for i,j in enumerate(words) :
             for num in range(len(words)):
-                if words[i] == words[num]:
-                    continue
-                else:
-                    if words[i] in words[num] and words[i] not in result:
-                        result.append(words[i])
+                if words[num] not in result:
+                    if words[i] == words[num]:
+                        continue
+                    else:
+                        if words[i] in words[num] and words[i] not in result:
+                            result.append(words[i])
 
         return result 

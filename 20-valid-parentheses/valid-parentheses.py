c3af0f853bc994df6991a89f9a58b6aad90299e9
Rishabh jain <91287627+Rishabh690@users.noreply.github.com>
2025-06-03 20:17:48 -0400

    Time: 2 ms (61.22%) | Memory: 12.4 MB (84.13%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,33 +1,33 @@
 class Solution(object):
     def isValid(self, s):
-        l = []
-        if s[0]==")" or s[0]=="]" or s[0]=='}':
-            return False
-        for i in s:
-            if i == '(' or i == '{' or i == '[':
-                l.append(i)
-            elif i == ')' and l and l[-1] == '(':
-                l.pop()
-            elif i == '}' and l and l[-1] == '{':
-                l.pop()
-            elif i == ']' and l and l[-1] == '[':
-                l.pop()
-            else:
-                return False
-        return not l
+        # l = []
+        # if s[0]==")" or s[0]=="]" or s[0]=='}':
+        #     return False
+        # for i in s:
+        #     if i == '(' or i == '{' or i == '[':
+        #         l.append(i)
+        #     elif i == ')' and l and l[-1] == '(':
+        #         l.pop()
+        #     elif i == '}' and l and l[-1] == '{':
+        #         l.pop()
+        #     elif i == ']' and l and l[-1] == '[':
+        #         l.pop()
+        #     else:
+        #         return False
+        # return not l
             
         
-        # result=[]
-        # poojan = {")":"(","]":"[","}":"{"}
+        result=[]
+        poojan = {")":"(","]":"[","}":"{"}
 
-        # for i in s:
-        #     if i in poojan:
-        #         if result and result[-1] == poojan[i]:
-        #             result.pop()
-        #         else:
-        #             return False
-        #     else:
-        #         result.append(i)
-        # return True if not result else False
+        for i in s:
+            if i in poojan:
+                if result and result[-1] == poojan[i]:
+                    result.pop()
+                else:
+                    return False
+            else:
+                result.append(i)
+        return True if not result else False
         
         

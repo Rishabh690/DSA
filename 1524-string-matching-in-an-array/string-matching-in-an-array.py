ddb5881ab3940ed6ac9449337e8d127883c1a0d8
Rishabh jain <91287627+Rishabh690@users.noreply.github.com>
2025-06-04 17:21:36 -0400

    Time: 3 ms (76.34%) | Memory: 12.5 MB (10.50%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.py
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.py
@@ -1,13 +1,18 @@
 class Solution(object):
     def stringMatching(self, words):
-        result=[]
-        for i,j in enumerate(words) :
-            for num in range(len(words)):
-                if words[num] not in result:
-                    if words[i] == words[num]:
-                        continue
-                    else:
-                        if words[i] in words[num] and words[i] not in result:
-                            result.append(words[i])
+        return list({w1 for i, w1 in enumerate(words)
+                     for j, w2 in enumerate(words)
+                     if i != j and w1 in w2})
 
-        return result 
+        
+        # result=[]
+        # for i,j in enumerate(words) :
+        #     for num in range(len(words)):
+        #         if words[num] not in result:
+        #             if words[i] == words[num]:
+        #                 continue
+        #             else:
+        #                 if words[i] in words[num] and words[i] not in result:
+        #                     result.append(words[i])
+
+        # return result 

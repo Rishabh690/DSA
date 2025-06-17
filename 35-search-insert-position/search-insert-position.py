73a69d16a1154a940b32adbfa07d5949efa20464
Rishabh jain <91287627+Rishabh690@users.noreply.github.com>
2025-06-17 16:12:15 -0400

    Time: 0 ms (100.00%) | Memory: 13.1 MB (23.54%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -1,22 +1,22 @@
 class Solution(object):
-    def searchInsert(self, nums, target):
-        if target in nums:
-            return nums.index(target)
-        else:
-            nums.append(target)
-            nums.sort()
-            return nums.index(target)
+    def searchInsert(self, nums, target):        
+        left, right = 0 , len(nums)-1
+        while left<= right:
+            m=(left + right) //2
+            if nums[m] == target:
+                return m
+            elif nums[m] < target:
+                left = m+1
+            else:
+                right = m-1
+        return left        
 
-        # left, right = 0 , len(nums)-1
 
-        # while left<= right:
-        #     m=(left + right) //2
-        #     if nums[m] == target:
-        #         return m
-        #     if nums[left] <=nums[middle] :
-        #         if  target < nums[left]:
-        #             left = m+1
-        #         else:
-        #             right = m - 1
-        #     else:
 
+
+        # if target in nums:
+        #     return nums.index(target)
+        # else:
+        #     nums.append(target)
+        #     nums.sort()
+        #     return nums.index(target)

ebcd2044706f2a55fe174d3070e4340ac7a3e397
Rishabh jain <91287627+Rishabh690@users.noreply.github.com>
2025-07-07 17:48:20 -0400

    Time: 5 ms (74.67%) | Memory: 13.6 MB (64.12%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,13 +1,23 @@
 class Solution(object):
     def majorityElement(self, nums):
-        n=(len(nums) / 2)
-        res= {}
-        for i in nums:
-            if i in res:
-                res[i] +=1
-            else:
-                res[i] = 1
-        for key,value in res.items():
-            if value > n:
-                return key
+        count= 0 
+        candidate = None
+
+        for num in nums:
+            if count == 0 :
+                candidate = num
+            count+=(1 if num == candidate else -1)
+        return candidate
+        
+        
+        # n=(len(nums) / 2)
+        # res= {}
+        # for i in nums:
+        #     if i in res:
+        #         res[i] +=1
+        #     else:
+        #         res[i] = 1
+        # for key,value in res.items():
+        #     if value > n:
+        #         return key
 
